fd8f57d41739d3bafe27a245728aa2bef8ae58e9
Mathemalsky <jurek.rostalsky@online.de>
2022-09-15 19:41:03 +0200

    fix bug

--- src/chacha.cpp
+++ src/chacha.cpp
@@ -8,12 +8,21 @@ void ChaCha::init(const void* key, const uint32_t counterOffset) {
   pState[12] += counterOffset;
 }
 
-#ifdef __SSE3__
+#include <iostream>
+static void printState(const uint32_t* state) {
+  printf("%11d %11d %11d %11d \n", state[0], state[4], state[8], state[12]);
+  printf("%11d %11d %11d %11d \n", state[1], state[5], state[9], state[13]);
+  printf("%11d %11d %11d %11d \n", state[2], state[6], state[10], state[14]);
+  printf("%11d %11d %11d %11d \n", state[3], state[7], state[11], state[15]);
+  printf("\n");
+}
+
+#if defined __SSE3__
 
 #include <immintrin.h>
 static inline void rotleft(__m128i& a, unsigned int count) {
   // can be optimized in case  count is divisible by 8
-  a = _mm_or_epi32(_mm_slli_epi32(a, count), _mm_srli_epi32(a, 32 - count));
+  a = _mm_or_si128(_mm_slli_epi32(a, count), _mm_srli_epi32(a, 32 - count));
 }
 
 static void inline quaterRound(__m128i& a, __m128i& b, __m128i& c, __m128i& d) {
@@ -22,7 +31,7 @@ static void inline quaterRound(__m128i& a, __m128i& b, __m128i& c, __m128i& d) {
   rotleft(d, 16);
 
   c = _mm_add_epi32(c, d);
-  b = _mm_xor_epi32(b, c);
+  b = _mm_xor_si128(b, c);
   rotleft(b, 12);
 
   a = _mm_add_epi32(a, b);
@@ -30,35 +39,51 @@ static void inline quaterRound(__m128i& a, __m128i& b, __m128i& c, __m128i& d) {
   rotleft(d, 8);
 
   c = _mm_add_epi32(c, d);
-  b = _mm_xor_epi32(b, c);
+  b = _mm_xor_si128(b, c);
   rotleft(b, 7);
 }
 
 void ChaCha::operator()(uint32_t* workingState) {
   std::memcpy(&workingState[0], &pState[0], 64);
 
-  __m128i a = _mm_load_si128((__m128i*) &workingState[0]);
-  __m128i b = _mm_load_si128((__m128i*) &workingState[4]);
-  __m128i c = _mm_load_si128((__m128i*) &workingState[8]);
-  __m128i d = _mm_load_si128((__m128i*) &workingState[12]);
-
   for (unsigned int i = 0; i < DOUBLE_ROUNDS; ++i) {
+    // printState(workingState);
     // uneven rounds
+
+    __m128i a = _mm_load_si128((__m128i*) &workingState[0]);
+    __m128i b = _mm_load_si128((__m128i*) &workingState[4]);
+    __m128i c = _mm_load_si128((__m128i*) &workingState[8]);
+    __m128i d = _mm_load_si128((__m128i*) &workingState[12]);
 
     quaterRound(a, b, c, d);
 
     // even rounds
 
     // maybe fatser with shuffle
+
     _mm_store_si128((__m128i*) &workingState[0], a);
     _mm_store_si128((__m128i*) &workingState[4], b);
     _mm_store_si128((__m128i*) &workingState[8], c);
     _mm_store_si128((__m128i*) &workingState[12], d);
 
-    a = _mm_set_epi32(workingState[0], workingState[5], workingState[10], workingState[15]);
-    b = _mm_set_epi32(workingState[1], workingState[6], workingState[11], workingState[12]);
-    c = _mm_set_epi32(workingState[2], workingState[7], workingState[8], workingState[13]);
-    d = _mm_set_epi32(workingState[3], workingState[4], workingState[9], workingState[14]);
+    // printState(workingState);
+
+    /*
+        a = _mm_set_epi32(workingState[3], workingState[2], workingState[1], workingState[0]);
+        b = _mm_set_epi32(workingState[4], workingState[7], workingState[6], workingState[5]);
+        c = _mm_set_epi32(workingState[9], workingState[8], workingState[11], workingState[10]);
+        d = _mm_set_epi32(workingState[14], workingState[13], workingState[12], workingState[15]);
+    */
+
+    b = _mm_shuffle_epi32(b, 0b00111001);
+    c = _mm_shuffle_epi32(c, 0b01001110);
+    d = _mm_shuffle_epi32(d, 0b10010011);
+
+    _mm_store_si128((__m128i*) &workingState[0], a);
+    _mm_store_si128((__m128i*) &workingState[4], b);
+    _mm_store_si128((__m128i*) &workingState[8], c);
+    _mm_store_si128((__m128i*) &workingState[12], d);
+    // printState(workingState);
 
     quaterRound(a, b, c, d);
 
@@ -66,6 +91,19 @@ void ChaCha::operator()(uint32_t* workingState) {
     _mm_store_si128((__m128i*) &workingState[4], b);
     _mm_store_si128((__m128i*) &workingState[8], c);
     _mm_store_si128((__m128i*) &workingState[12], d);
+
+    // printState(workingState);
+
+    b = _mm_shuffle_epi32(b, 0b10010011);
+    c = _mm_shuffle_epi32(c, 0b01001110);
+    d = _mm_shuffle_epi32(d, 0b00111001);
+
+    _mm_store_si128((__m128i*) &workingState[4], b);
+    _mm_store_si128((__m128i*) &workingState[8], c);
+    _mm_store_si128((__m128i*) &workingState[12], d);
+
+    // printState(workingState);
+    // exit(0);
   }
 
   // add original state to current
@@ -82,9 +120,7 @@ void ChaCha::operator()(uint32_t* workingState) {
 // implementation related to:
 // https://stackoverflow.com/questions/776508/best-practices-for-circular-shift-rotate-operations-in-c
 static inline uint32_t rotleft(const uint32_t integer, unsigned int count) {
-  const unsigned int cutof = 31;
-  count &= cutof;  // shifts larger than 31 bits are not needed in a 32 bit integer
-  return (integer << count | integer >> ((-count) & cutof));
+  return (integer << count | integer >> (32 - count));
 }
 
 static inline void quaterRound(uint32_t& a, uint32_t& b, uint32_t& c, uint32_t& d) {
@@ -110,16 +146,22 @@ void ChaCha::operator()(uint32_t* workingState) {
 
   for (unsigned int i = 0; i < DOUBLE_ROUNDS; ++i) {
     // uneven rounds
+    // printState(workingState);
     quaterRound(workingState[0], workingState[4], workingState[8], workingState[12]);
     quaterRound(workingState[1], workingState[5], workingState[9], workingState[13]);
     quaterRound(workingState[2], workingState[6], workingState[10], workingState[14]);
     quaterRound(workingState[3], workingState[7], workingState[11], workingState[15]);
+
+    // printState(workingState);
 
     // even rounds
     quaterRound(workingState[0], workingState[5], workingState[10], workingState[15]);
     quaterRound(workingState[1], workingState[6], workingState[11], workingState[12]);
     quaterRound(workingState[2], workingState[7], workingState[8], workingState[13]);
     quaterRound(workingState[3], workingState[4], workingState[9], workingState[14]);
+
+    // printState(workingState);
+    // exit(0);
   }
 
   // add original state to current
